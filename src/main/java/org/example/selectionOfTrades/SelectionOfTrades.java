d6966654ada9bef507bfcd92a28ed4150d47f6f8
bratello-0 <bro4bet69K@yandex.ru>
2023-05-08 14:47:20 +0400

    0.2.1.3 add multi properties(dev, product).

--- src/main/java/org/example/selectionOfTrades/SelectionOfTrades.java
+++ src/main/java/org/example/selectionOfTrades/SelectionOfTrades.java
@@ -1,10 +1,7 @@
 package org.example.selectionOfTrades;
 
-import org.example.selectionOfTrades.configs.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 
